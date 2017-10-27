fc083ef8eac877befc23b55cc40703e92dc5e368
bostrich <13057506020@163.com>
2017-10-27 18:51:26 +0800

    更新

--- clean/src/main/java/com/syezon/clean/QQCleanActivity.java
+++ clean/src/main/java/com/syezon/clean/QQCleanActivity.java
@@ -1,14 +1,23 @@
 package com.syezon.clean;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.syezon.clean.bean.QQCacheBean;
+import com.syezon.clean.utils.QQScanUtils;
+
+import java.util.List;
 
 /**
  * QQ 接收的文件所在文件夹 /tencent/QQfile_recv
@@ -21,8 +30,11 @@ import android.widget.TextView;
 
 public class QQCleanActivity extends AppCompatActivity {
 
+    private static final int GET_TALKING_CACHE = 11;
     private ImageView imgLoadedGarbage,  imgLoadedTalking;
     private DotRotateLoadingView customLoadingGarbage, customLoadingTalking;
+    private LinearLayout llContainerItems;
+
     private Handler mHandler;
 
     @Override
@@ -43,9 +55,13 @@ public class QQCleanActivity extends AppCompatActivity {
         //TODO 扫描垃圾缓存 垃圾文件、临时文件
 
         //TODO 扫描聊天文件 包括聊天视频，聊天文件
-
-
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<QQCacheBean> list = QQScanUtils.getTalkingCacheFile();
+                mHandler.sendEmptyMessage(GET_TALKING_CACHE);
+            }
+        }).start();
     }
 
     private void initHandler() {
@@ -53,11 +69,17 @@ public class QQCleanActivity extends AppCompatActivity {
             @Override
             public void handleMessage(Message msg) {
                 switch(msg.what){
-
+                    case GET_TALKING_CACHE:
+                        setScanResultViews();
+                        break;
+                    default:
+                        break;
                 }
             }
         };
     }
+
+
 
     private void initViews() {
         imgLoadedGarbage = (ImageView) findViewById(R.id.img_loaded_garbage);
@@ -68,5 +90,19 @@ public class QQCleanActivity extends AppCompatActivity {
         customLoadingGarbage.startRotating();
         customLoadingTalking.startRotating();
 
+        llContainerItems = (LinearLayout) findViewById(R.id.ll_container_items);
+
+    }
+
+    private void setScanResultViews() {
+        llContainerItems.removeAllViews();
+        View view = LayoutInflater.from(this).inflate(R.layout.item_qq_scan_result, null, false);
+        RelativeLayout rlTalkingImg = (RelativeLayout) view.findViewById(R.id.rl_talking_imgs);
+        RelativeLayout rlVideo = (RelativeLayout) view.findViewById(R.id.rl_video);
+        RelativeLayout rlTalkingSound = (RelativeLayout) view.findViewById(R.id.rl_talking_sound);
+        RelativeLayout rlReceive = (RelativeLayout) view.findViewById(R.id.rl_receive);
+
+
+        llContainerItems.addView(view);
     }
 }
--- clean/src/main/java/com/syezon/clean/utils/QQScanUtils.java
+++ clean/src/main/java/com/syezon/clean/utils/QQScanUtils.java
@@ -2,7 +2,11 @@ package com.syezon.clean.utils;
 
 import android.os.Environment;
 
+import com.syezon.clean.bean.QQCacheBean;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * QQ 接收的文件所在文件夹 /tencent/QQfile_recv
@@ -15,16 +19,96 @@ import java.io.File;
 
 public class QQScanUtils {
 
-    public static void getTalkingCacheFile(){
+    public static List<QQCacheBean> getTalkingCacheFile(){
         File storage = Environment.getExternalStorageDirectory();
         File file_qq = new File(storage, "MobileQQ");
+        List<QQCacheBean> list = new ArrayList<>();
         if(file_qq.exists()){//判断文件是否存在
+            //扫描聊天图片
+            File file_talking = new File(file_qq, "photo");
+            if(file_talking.exists()){
 
 
+            }
 
+            //扫描视频文件
+            File file_video = new File(file_qq, "shortvideo");
+            if(file_video.exists()){
+                list.addAll(getFile(file_video, "shortvideo"));
+            }
 
-
+            //扫描语音文件
+            File file_voice = new File(file_qq, "ptt");
+            if(file_video.exists()){
+                list.addAll(getFile(file_voice, "ptt"));
+            }
         }
+        return list;
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     */
+    public static List<QQCacheBean> getFile(File file, String type) {
+        File[] fileArray = file.listFiles();
+        List<QQCacheBean> mFileList = new ArrayList<>();
+        if (fileArray != null) {
+            for (File f : fileArray) {
+                if (f.isFile()) {
+                    long size = f.length();
+                    switch(type){
+                        case "photo":
+                            if(f.getName().endsWith(".png")){
+                                QQCacheBean bean = new QQCacheBean();
+                                bean.setFile(f);
+                                bean.setType("phote");
+                                bean.setFileType("png");
+                                bean.setSize(f.length());
+                                mFileList.add(bean);
+                            }
+                            break;
+                        case "ptt":
+                            if(f.getName().endsWith(".amr")){
+                                QQCacheBean bean = new QQCacheBean();
+                                bean.setFile(f);
+                                bean.setType("ptt");
+                                bean.setFileType("amr");
+                                bean.setSize(f.length());
+                                mFileList.add(bean);
+                            }
+                            break;
+
+                        case "shortvideo":
+                            if(f.getName().endsWith(".mp4")){
+                                QQCacheBean bean = new QQCacheBean();
+                                bean.setFile(f);
+                                bean.setType("shortvideo");
+                                bean.setFileType("mp4");
+                                bean.setSize(f.length());
+                                mFileList.add(bean);
+                            }else if(f.getName().endsWith(".png")){
+                                QQCacheBean bean = new QQCacheBean();
+                                bean.setFile(f);
+                                bean.setType("shortvideo");
+                                bean.setFileType("png");
+                                bean.setSize(f.length());
+                                mFileList.add(bean);
+                            }
+                            break;
+                        default://默认不处理
+
+                            break;
+                    }
+
+                } else {
+                    List<QQCacheBean> files_tem = getFile(f, type);
+                    if (files_tem.size() > 0) mFileList.addAll(files_tem);
+                }
+            }
+        }
+        return mFileList;
     }
 
 }
